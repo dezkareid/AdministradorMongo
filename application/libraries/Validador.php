0673cb238f0468a4a69b5ca94c68d8ca8d6d054c
dezkareid <elmaildeldezkareid@gmail.com>
2013-05-02 02:01:17 -0500

    solo faltan 2 validaciones :D

--- application/libraries/Validador.php
+++ application/libraries/Validador.php
@@ -2,7 +2,56 @@
 
 class Validador {
 
+	public function __construct() {
 	
+	}
+
+	public function eliminaCaracteresEspeciales($cadena)
+	{
+		$cadena=preg_replace("/!|;|-|{|}|\t|\n|\r/","",$cadena);
+		filter_var($cadena FILTER_SANITIZE_STRING,FILTER_SANITIZE_STRIPPED);
+	}
+
+	public function eliminaEspacios($cadena)
+	{
+		$cadena= trim($cadena);
+		$cadena = preg_replace("/ +/"," ",$cadena);	
+		return $cadena;
+	}
+
+	public function validaNombre($cadena)
+	{
+		return preg_match('/^[a-z áéíóúñ]/i', $cadena);
+	}
+
+	public function validaUsuario($cadena)
+	{
+		# code...
+	}
+
+	public function validaPassword($value='')
+	{
+		
+	}
+
+	public function validaEmail($cadena)
+	{
+		return filter_var($email, FILTER_VALIDATE_EMAIL);
+	}
+
+	public function validaNumero($cadena)
+	{
+		return filter_var($nume, FILTER_VALIDATE_INT);
+	}
+
+	public function validaFlotante($cadena)
+	{
+		$puntos=substr_count($cadena, '.');
+		if($puntos==0||$puntos>1)
+			return false;
+		return filter_var($nume, FILTER_VALIDATE_FLOAT);
+
+	}
 
 }
 
